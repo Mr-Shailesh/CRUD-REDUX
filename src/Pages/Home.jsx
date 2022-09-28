78c5a32dd936433874a7d397be37464a86f9a74c
Mr-Shailesh <shaileshg7860@gmail.com>
2022-09-28 18:39:31 +0530

    Changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
-// import Table from "../Components/Table";
 import TableR from "../Components/TableR";
 
 const Home = () => {
@@ -8,7 +7,6 @@ const Home = () => {
     <div>
       <Container>
         <h2 className="d-flex justify-content-center">List of Users</h2>
-        {/* <Table /> */}
         <TableR />
       </Container>
     </div>
